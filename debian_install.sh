c0f1bced2d7c5522be9b13fa02668e5d4d062507
Damien Mandrioli <dmandrioli@gmail.com>
2019-10-09 20:23:25 +0200

    Update debian_install.sh

--- debian_install.sh
+++ debian_install.sh
@@ -2,24 +2,13 @@
 apt-get update
 apt-get -y install git
 useradd -g users -d /home/ssedemo -s /bin/bash ssedemo
-rm -rf /home/ssedemo
 mkdir /home/ssedemo
 chown ssedemo:users /home/ssedemo
 curl https://install.meteor.com/ | sh
 cd /home/ssedemo
 sudo -u ssedemo git clone https://github.com/Hitachi-Automotive-And-Industry-Lab/semantic-segmentation-editor
-mkdir images
-chown ssedemo:users images
-cd images
-mkdir samples
-chown ssedemo:users samples
-cd samples
-sudo -u ssedemo wget https://github.com/dmandrioli/sse-extra/raw/master/pointcloud.pcd
-sudo -u ssedemo wget https://github.com/dmandrioli/sse-extra/raw/master/bitmap.png
-cd ../../semantic-segmentation-editor
+cd semantic-segmentation-editor
 sudo -u ssedemo meteor npm install
-sudo -u ssedemo sed 's_\(input-folder.*\)XXXXX_\1/home/ssedemo/images_' -i /home/ssedemo/semantic-segmentation-editor/settings.json
-sudo -u ssedemo sed 's_\(output-folder.*\)XXXXX_\1/home/ssedemo/pcd_' -i /home/ssedemo/semantic-segmentation-editor/settings.json
 sudo -u ssedemo sed 's/\(.*saveData.*\){/\1{return;/' -i /home/ssedemo/semantic-segmentation-editor/server/main.js
 sudo -u ssedemo sed 's/\(.*\/save.*\){/\1{return;/' -i /home/ssedemo/semantic-segmentation-editor/server/files.js
 cat > /home/ssedemo/semantic-segmentation-editor/client/g.html << EOL
@@ -35,6 +24,6 @@ cat > /home/ssedemo/semantic-segmentation-editor/client/g.html << EOL
 </script>
 </head>
 EOL
-iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
+iptables -t nat -A PREROUTING -i ens2 -p tcp --dport 80 -j REDIRECT --to-port 3000
 killall -9 node mongod
-while :; do sudo -u ssedemo meteor --production --settings settings.json; done&
+while :; do sudo -u ssedemo meteor --settings settings.json; done&
